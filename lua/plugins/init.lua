17c5b4ddf3b0dfbc170ece844f0e847c11444779
MrSydar <denys.hromniuk@gmail.com>
2022-05-29 16:46:23 +0200

    refactor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,12 +1,12 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
+
 	use 'shaunsingh/nord.nvim'
 
 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 
 	use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
-	use {
-		'nvim-lualine/lualine.nvim',
+	use {'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
 end)
